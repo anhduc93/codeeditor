47f1d7378a99df83beb34fe6172740960733b345
Duc Do <duc@skvare.com>
2014-10-08 22:00:03 -0500

    Test deployment

--- helloworld.py
+++ helloworld.py
@@ -4,7 +4,9 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
+		self.response.write('This is a test')
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
+	('/', SecondPage),
 ], debug=True)
