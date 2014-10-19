1aae92068140c3ff2a1c8784d87efa58e4e3a59b
Duc Do <duc@skvare.com>
2014-10-19 14:41:47 -0500

    Test Form 3b

--- codeeditor.py
+++ codeeditor.py
@@ -1,17 +1,17 @@
-import cgi
+ import cgi
 from google.appengine.api import users
 import webapp2
 
-MAIN_PAGE_HTML = """\
+MAIN_PAGE_HTML = "\
 <html>
 	<body>
-		<form action = "/sign" method = "post">
-			<div><textarea name = "content" rows="3" cols="60"></textarea></div>
-			<div><input type="submit" value="Sign"</div>
+		<form action = '/sign' method = 'post'>
+			<div><textarea name = 'content' rows='3' cols='60'></textarea></div>
+			<div><input type='submit' value='Sign'</div>
 		</form>
 	</body>
 </html>
-"""
+"
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
@@ -26,4 +26,4 @@ class Codeeditor(webapp2.RequestHandler):
 application = webapp2.WSGIApplication([
 	('/', MainPage),
 	('/sign', Codeeditor),
-], debug=True)
+], debug=True) 
