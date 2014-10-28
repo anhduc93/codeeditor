4d426d56b9e77a4c8670dbe0424971e98f3981ae
Duc Do <duc@skvare.com>
2014-10-27 20:43:54 -0500

    Quick Demo

--- codeeditor.py
+++ codeeditor.py
@@ -12,24 +12,31 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 	extensions=['jinja2.ext.autoescape'],
 	autoescape=True)
 
-class MainPage(webapp2.RequestHandler):
+# Main Page Handler
+class MainPageHandler(webapp2.RequestHandler):
 	def get(self):
-		current_user = False
+		is_current_user = False
+		user_nickname = ""
+		# Check if this is the current user
 		if users.get_current_user():
-			current_user = True
+			is_current_user = True
+			user = users.get_current_user()
 			url = users.create_logout_url(self.request.uri)
 			url_linktext = 'Logout'
+			user_nickname = user.nickname()
 		else:
 			url = users.create_login_url(self.request.uri)
 			url_linktext = 'Login'
 		template_values={
-			'current_user':current_user,
+			'is_current_user': is_current_user,
 			'url' :url,
 			'url_linktext': url_linktext,
+			'user_nickname':user_nickname,
 		}
 		template = JINJA_ENVIRONMENT.get_template('templates/index.html')
 		self.response.write(template.render(template_values))
 
+# Model
 DEFAULT_FILE_NAME = 'default_filename'
 def codefile_key(file_name=DEFAULT_FILE_NAME):
 	"""Constructs a Datastore key for a File entity with file_name."""
@@ -41,29 +48,65 @@ class CodeFile(ndb.Model):
 	content = ndb.TextProperty(indexed=False)
 	date = ndb.DateTimeProperty(auto_now_add=True)
 	
-# Handling file-save event
-class CodeFileSave(webapp2.RequestHandler):
+# Handle save file event
+class CodeFileSaveHandler(webapp2.RequestHandler):
 	def post(self):
-		file_name = self.request.get('file_name', DEFAULT_FILE_NAME)	# get the file name textfield
+		file_name = self.request.get('file_name', DEFAULT_FILE_NAME)	# get the "file_name" text field
 		code_file = CodeFile(parent = codefile_key(file_name))
 		
+		# Check and pass appropriate user data
+		is_current_user = False
+		user_nickname=""
 		if users.get_current_user():
+			# Save this file if this is the current user
+			is_current_user = True
 			code_file.author = users.get_current_user()
 			code_file.content = self.request.get('file_content')
 			code_file.title = file_name
 			code_file.put()
-			self.response.write("You have successfully saved this file to your account!")
+			success_text = "Save successfully!"
 
-# Lists all file
+			url = users.create_logout_url(self.request.uri)
+			url_linktext = 'Logout'
+			user = users.get_current_user()
+			user_nickname = user.nickname()
+
+			template_values={
+				'url_linktext' : url_linktext,
+				'url' : url,
+				'user_nickname' : user_nickname,
+				'is_current_user': is_current_user,
+				'success_text':success_text,
+			}
+			template = JINJA_ENVIRONMENT.get_template('templates/save.html')
+			self.response.write(template.render(template_values))
+
+			
+# Lists all files belong to the current user
 class CodeFileListHandler(webapp2.RequestHandler):
 	def get(self):
 		file_name = self.request.get('file_name', DEFAULT_FILE_NAME)
-		self.response.write("This is the list of all files!")
-		query = CodeFile.query(
-			ancestor=codefile_key(file_name)).order(-CodeFile.date)
+		
+		is_current_user = False
+		user_nickname = ""
+		if users.get_current_user():
+			is_current_user = True
+			user = users.get_current_user()
+			url = users.create_logout_url(self.request.uri)
+			url_linktext = 'Logout'
+			user_nickname = user.nickname()
+		else:
+			url = users.create_login_url(self.request.uri)
+			url_linktext = 'Login'
+		
+		query = CodeFile.query(CodeFile.author==users.get_current_user()).order(-CodeFile.date)
 		codefiles = query.fetch(10)
 		
 		template_values={
+			'url_linktext' : url_linktext,
+			'url':url,
+			'user_nickname' : user_nickname,
+			'is_current_user': is_current_user,
 			'codefiles': codefiles,
 		}
 		template = JINJA_ENVIRONMENT.get_template('templates/list.html')
@@ -72,14 +115,7 @@ class CodeFileListHandler(webapp2.RequestHandler):
 
 # Routing
 application = webapp2.WSGIApplication([
-	('/', MainPage),
+	('/', MainPageHandler),
 	('/list', CodeFileListHandler),
-	('/save', CodeFileSave),
+	('/save', CodeFileSaveHandler),
 ], debug=True)
-
-"""
-	Pass JavaScript variable to Python
-	Save into the database
-	Load the file and display to the mainpage
-	Design the interface
-"""
